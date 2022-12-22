c438575e21338a11dd8f5065272c04749bc148b2
DouglasMesquita <douglasrm.azevedo@gmail.com>
2022-12-22 19:37:40 +0100

    Adding functions to handle individual studies' files

--- app/logic/utils/utils_data.R
+++ app/logic/utils/utils_data.R
@@ -11,6 +11,10 @@ box::use(
     summarise,
     ungroup,
   ],
+  rlang[
+    `!!`,
+    parse_expr,
+  ],
   sf[
     st_as_sf,
     st_simplify,
@@ -30,7 +34,7 @@ summarise_studies <- function(studies) {
     summarise(
       flag = first(flag),
       n_mpas = n_distinct(name),
-      n_studies = n_distinct(ID2),
+      n_studies = n_distinct(ID),
       n_votes = n(),
       n_positive = sum(direction == "positive"),
       n_negative = sum(direction == "negative"),
@@ -74,4 +78,50 @@ summarise_mechanism <- function(studies, mechanism) {
     )
 
   return(ns)
+}
+
+#' @title
+#' @description
+#'
+#' @param file
+#'
+#' @export
+read_csv_wrap <- function(file) {
+  read.csv(file = file, colClasses = "character", na.strings = "")
+}
+
+#' @title
+#' @description
+#'
+#' @param pathway_list
+#'
+#' @export
+clean_pathway <- function(pathway_list) {
+  # collect info
+  pathway_file_in <- pathway_list$file_in
+  pathway_file <- pathway_list$file
+  pathway_fill <- pathway_list$label
+  pathway_filter <- pathway_list$filter
+  pathway_file_in <- pathway_list$file_in
+
+  # filter papers that are participating
+  df <- read_csv_wrap(file = pathway_file_in) %>%
+    filter(!is.na(direction))
+
+  # filter pathway
+  if (!is.null(pathway_filter))
+    df <- filter(.data = df, !!parse_expr(pathway_filter))
+
+  # fill pathway
+  if (!is.null(pathway_fill))
+    df <- mutate(.data = df, mechanism = pathway_fill)
+
+  # try to save the file
+  tryCatch(
+    expr = {
+      write.csv(x = df, file = pathway_file)
+      return(pathway_file)
+    },
+    error = function(e) e
+  )
 }
